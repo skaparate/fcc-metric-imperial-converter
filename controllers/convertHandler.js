e8d1dea4f1cbe13a2d941dd9770c32cb8ef0c76e
Nicolas Mancilla Vergara <nico@nicomv.com>
2020-08-29 14:04:39 -0400

    updated syntax and cleanup

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,52 +1,34 @@
 /*
-*
-*
-*       Complete the handler logic below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the handler logic below
+ *
+ *
+ */
 
+/**
+ * Controller in charge of converting between
+ * metric and imperial units.
+ */
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    var result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    var result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    var result;
-    
-    return result;
-  };
+  this.getNum = function (input) {};
 
-  this.spellOutUnit = function(unit) {
-    var result;
-    
-    return result;
-  };
-  
-  this.convert = function(initNum, initUnit) {
+  this.getUnit = function (input) {};
+
+  this.getReturnUnit = function (initUnit) {};
+
+  this.spellOutUnit = function (unit) {};
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
-    
+    let result;
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-    
-    return result;
-  };
-  
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {};
 }
 
 module.exports = ConvertHandler;
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const cors        = require('cors');
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
 
-var app = express();
+const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -46,9 +45,8 @@ app.listen(process.env.PORT || 3000, function () {
       try {
         runner.run();
       } catch(e) {
-        var error = e;
           console.log('Tests are not valid:');
-          console.log(error);
+          console.log(e);
       }
     }, 1500);
   }
