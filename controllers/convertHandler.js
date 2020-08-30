db2a3544d37baf3d848519205e11c769fad72856
Nicolas Mancilla Vergara <nico@nicomv.com>
2020-08-29 20:38:12 -0400

    added test logic

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -6,26 +6,173 @@
  *
  */
 
+const { init } = require("../server");
+
+class ValidationError extends Error {
+  constructor(msg, input, reason = "") {
+    super(msg);
+    this.input = input;
+    this.reason = reason;
+  }
+}
+
 /**
  * Controller in charge of converting between
  * metric and imperial units.
  */
 function ConvertHandler() {
-  this.getNum = function (input) {};
+  const galToL = 3.78541,
+    lbsToKg = 0.453592,
+    miTokm = 1.60934;
+  const units = {
+    gal: {
+      convertsTo: "l",
+      spell: "gallons",
+    },
+    l: {
+      convertsTo: "gal",
+      spell: "litres",
+    },
+    mi: {
+      convertsTo: "km",
+      spell: "miles",
+    },
+    km: {
+      convertsTo: "mi",
+      spell: "kilometers",
+    },
+    lbs: {
+      convertsTo: "kg",
+      spell: "pounds",
+    },
+    kg: {
+      convertsTo: "lbs",
+      spell: "kilograms",
+    },
+  };
+  const regex = new RegExp(/([\d\.\/]+)?\s*([a-z]+)$/, "i");
 
-  this.getUnit = function (input) {};
+  function inputToNumber(input) {
+    console.debug("Input to number:", input);
+    let num;
+    try {
+      num = parseFloat(input);
+    } catch (e) {
+      throw new ValidationError("invalid number", input);
+    }
 
-  this.getReturnUnit = function (initUnit) {};
+    console.debug("Parsed number:", num);
+    return num;
+  }
 
-  this.spellOutUnit = function (unit) {};
+  /**
+   * Parses the input data and converts it to an object with
+   * two properties:
+   *
+   * unit: the unit found on the data,
+   * number: the parsed number.
+   *
+   * @param {String} data The data to parse.
+   * @throws Error if the unit or number are not valid.
+   */
+  this.parse = function (data) {
+    console.debug("Parsing user input:", data);
+    if (!data) {
+      throw new ValidationError(
+        "invalid number and unit",
+        input,
+        "no data passed"
+      );
+    }
+
+    const input = data.trim().toLowerCase();
+    const match = input.match(regex);
+    console.debug("Matched result:", match);
+
+    if (!match) {
+      throw new ValidationError("invalid number and unit", input);
+    }
+
+    if (!units.hasOwnProperty(match[2])) {
+      throw new ValidationError("invalid unit", input);
+    }
+
+    console.debug("Unit:", match[2]);
+    let ms = 1;
+
+    if (match[1]) {
+      const slashIndex = match[1].indexOf("/");
+      if (slashIndex === 0) {
+        throw new ValidationError("invalid number", input, "empty fraction");
+      }
+      if (slashIndex > 0) {
+        const split = match[1].split("/");
+        if (split.length > 2) {
+          throw new ValidationError(
+            "invalid number",
+            input,
+            "more than 1 fraction"
+          );
+        }
+        const left = inputToNumber(split[0]);
+        const right = inputToNumber(split[1]);
+        ms = left / right;
+      } else {
+        ms = inputToNumber(match[1]);
+      }
+    }
+    const result = {
+      unit: match[2],
+      number: ms,
+    };
+    console.debug("Parsed result:", result);
+    return result;
+  };
+
+  this.getNum = function (input) {
+    return this.parse(input).number;
+  };
+
+  this.getUnit = function (input) {
+    return this.parse(input).unit;
+  };
+
+  this.getReturnUnit = function (initUnit) {
+    const prop = units[initUnit];
+    if (!prop) {
+      console.error("The initial unit was not found:", initUnit);
+      throw new ValidationError("invalid unit", initUnit);
+    }
+    return prop.convertsTo;
+  };
+
+  this.spellOutUnit = function (unit) {
+    const prop = units[unit];
+    if (prop === undefined) {
+      throw new ValidationError("invalid unit", unit);
+    }
+    return prop.spell;
+  };
 
   this.convert = function (initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
+    const toUnit = this.getReturnUnit(initUnit);
 
-    return result;
+    if (toUnit === "gal") {
+      return initNum / galToL;
+    }
+    if (toUnit === "l") {
+      return initNum * galToL;
+    }
+    if (toUnit === "mi") {
+      return initNum / miTokm;
+    }
+    if (toUnit === "km") {
+      return initNum * miTokm;
+    }
+    if (toUnit === "lbs") {
+      return initNum / lbsToKg;
+    }
+    return initNum * lbsToKg;
   };
 
   this.getString = function (initNum, initUnit, returnNum, returnUnit) {};
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -21,29 +21,56 @@ suite("Unit Tests", function () {
     });
 
     test("Decimal Input", function (done) {
-      //done();
+      const input = ["1.2gal", ".3gal", ".3l", ".1mi"];
+      const expected = [1.2, 0.3, 0.3, 0.1];
+
+      input.forEach(function (input, index) {
+        assert.equal(convertHandler.getNum(input), expected[index]);
+      });
+      done();
     });
 
     test("Fractional Input", function (done) {
-      //done();
+      const input = ["1/2gal", "2/3l", "3/1lbs", "20/10km"];
+      const expected = [0.5, 0.6666666666666666, 3, 2];
+      try {
+        input.forEach(function (input, index) {
+          assert.equal(convertHandler.getNum(input), expected[index]);
+        });
+      } catch (e) {
+        console.error(e);
+        return done(e);
+      }
+      done();
     });
 
     test("Fractional Input w/ Decimal", function (done) {
-      //done();
+      const input = ["1/2gal", "2.3/3l", "3/1l", ".2/.3km"];
+      const expected = [0.5, 0.7666666666666666, 3, 0.6666666666666667];
+      input.forEach(function (input, index) {
+        assert.equal(convertHandler.getNum(input), expected[index]);
+      });
+      done();
     });
 
     test("Invalid Input (double fraction)", function (done) {
-      //done();
+      const input = "1/2/3gal";
+      assert.throws(() => {
+        convertHandler.getNum(input);
+      }, "invalid number");
+      done();
     });
 
     test("No Numerical Input", function (done) {
-      //done();
+      const input = "gal";
+      assert.equal(convertHandler.getNum(input), 1);
+      done();
     });
   });
 
   suite("Function convertHandler.getUnit(input)", function () {
     test("For Each Valid Unit Inputs", function (done) {
-      var input = [
+      const input = [
         "gal",
         "l",
         "mi",
@@ -58,20 +85,25 @@ suite("Unit Tests", function () {
         "KG",
       ];
       input.forEach(function (ele) {
-        //assert
+        const result = convertHandler.getUnit(ele);
+        assert.equal(result, ele.toLowerCase());
       });
       done();
     });
 
     test("Unknown Unit Input", function (done) {
-      //done();
+      const input = "kb";
+      assert.throws(function () {
+        convertHandler.getUnit(input);
+      }, "invalid unit");
+      done();
     });
   });
 
   suite("Function convertHandler.getReturnUnit(initUnit)", function () {
     test("For Each Valid Unit Inputs", function (done) {
-      var input = ["gal", "l", "mi", "km", "lbs", "kg"];
-      var expect = ["l", "gal", "km", "mi", "kg", "lbs"];
+      const input = ["gal", "l", "mi", "km", "lbs", "kg"];
+      const expect = ["l", "gal", "km", "mi", "kg", "lbs"];
       input.forEach(function (ele, i) {
         assert.equal(convertHandler.getReturnUnit(ele), expect[i]);
       });
@@ -81,41 +113,92 @@ suite("Unit Tests", function () {
 
   suite("Function convertHandler.spellOutUnit(unit)", function () {
     test("For Each Valid Unit Inputs", function (done) {
-      //see above example for hint
+      const input = ["gal", "l", "mi", "km", "lbs", "kg"];
+      const expect = [
+        "gallons",
+        "litres",
+        "miles",
+        "kilometers",
+        "pounds",
+        "kilograms",
+      ];
+      input.forEach(function (ele, i) {
+        assert.equal(convertHandler.spellOutUnit(ele), expect[i]);
+      });
       done();
     });
   });
 
   suite("Function convertHandler.convert(num, unit)", function () {
     test("Gal to L", function (done) {
-      var input = [5, "gal"];
-      var expected = 18.9271;
-      assert.approximately(
-        convertHandler.convert(input[0], input[1]),
-        expected,
-        0.1
-      ); //0.1 tolerance
+      const input = [5, "gal"];
+      const expected = 18.9271;
+      try {
+        assert.approximately(
+          convertHandler.convert(input[0], input[1]),
+          expected,
+          0.1
+        ); //0.1 tolerance
+      } catch (e) {
+        console.error(e);
+        return done(e);
+      }
       done();
     });
 
     test("L to Gal", function (done) {
-      //done();
+      const input = [3, "l"];
+      const expected = 0.7925186;
+      assert.approximately(
+        convertHandler.convert(input[0], input[1]),
+        expected,
+        0.1
+      );
+      done();
     });
 
     test("Mi to Km", function (done) {
-      //done();
+      const input = [3.232, "mi"];
+      const expected = 5.2014;
+      assert.approximately(
+        convertHandler.convert(input[0], input[1]),
+        expected,
+        0.1
+      );
+      done();
     });
 
     test("Km to Mi", function (done) {
-      //done();
+      const input = [53.29, "km"];
+      const expected = 33.11287;
+      assert.approximately(
+        convertHandler.convert(input[0], input[1]),
+        expected,
+        0.1
+      );
+      done();
     });
 
     test("Lbs to Kg", function (done) {
-      //done();
+      const input = [3.9, "lbs"];
+      const expected = 1.76901;
+      assert.approximately(
+        convertHandler.convert(input[0], input[1]),
+        expected,
+        0.1
+      );
+      done();
     });
 
     test("Kg to Lbs", function (done) {
-      //done();
+      const input = [3.1111111111111107, "kg"];
+      const expected = 6.858825934640635;
+      assert.approximately(
+        convertHandler.convert(input[0], input[1]),
+        expected,
+        0.1
+      );
+      done();
     });
   });
 });
